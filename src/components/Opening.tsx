5a8c27dab2f729bf60f5de6ed3eb869d87e275ca
nakyoutae <qppk@naver.com>
2024-03-19 23:42:16 +0900

    - 메인 이미지 로드 방식 수정

--- src/components/Opening.tsx
+++ src/components/Opening.tsx
@@ -6,7 +6,8 @@ const Opening = () => {
         <div className="opening">
             <h1>the wedding of</h1>
             {/* <h1>if(🤵🏻🤍👰🏻‍♀️) → marriage</h1> */}
-            <div className="img" style={{backgroundImage: `url(${MyImage})`}}>
+            <div className="img">
+                <img src={MyImage} alt="" />
             </div>
             <div className="info">
                 <h2>나규태 그리고 최보영</h2>
