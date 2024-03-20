b7589208737eb45cab23cd948a7a8d5b97695daf
bychoi <bychoi@ddfactory.kr>
2024-03-20 16:23:58 +0900

    - 플로팅 버튼 동작 수정, 이미지 순서 변경, 신랑신부용 내용 교체

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -6,26 +6,7 @@ const Account = () => {
         <div className="box">
             <h2 className="ico" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">🤍</h2>
             <ul className="account" data-aos="fade-up" data-aos-anchor-placement="center-bottom" data-aos-easing="ease-in-out" data-aos-duration="800">
-                <li><h3>🤵🏻 신랑측 혼주 계좌번호</h3></li>
                 <li>
-                    <ul>
-                        <li>(부) 농협은행 352-1399-1795-23</li>
-                        <li>나성원</li>
-                    </ul>
-                    <div>
-                        <a onClick={() => onCopy('100050558487')}>계좌 복사하기</a>
-                    </div>
-                </li>
-                <li>
-                    <ul>
-                        <li>(모) 신한은행 110-393-499231</li>
-                        <li>성춘희</li>
-                    </ul>
-                    <div>
-                        <a onClick={() => onCopy('100022989148')}>계좌 복사하기</a>
-                    </div>
-                </li>
-                {/* <li>
                     <h3>🤵🏻 신랑 측 계좌번호</h3>
                     <ul>
                         <li>토스뱅크 1000-5055-8487</li>
@@ -46,7 +27,7 @@ const Account = () => {
                         <a onClick={() => onCopy('100022989148')}>계좌 복사하기</a>
                         <a onClick={() => onClick('https://link.kakaopay.com/_/0C7aeoq')}><i className="ic-kakaotalk"></i>카카오페이 송금하기</a>
                     </div>
-                </li> */}
+                </li>
             </ul>
         </div>
     )
